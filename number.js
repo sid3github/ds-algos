5ebef490d715083ebe375c074ef702de09946244
sid3github <siddharthpadwal3@gmail.com>
2018-07-09 19:09:21 +0530

    number is positive/negative

---
+++ number.js
@@ -0,0 +1,13 @@
+function number(n) {
+
+
+    if (n < 0) {
+        console.log('n is negative');
+    }
+    else if(n > 0) {
+        console.log('n is positive');
+    }else{
+        console.log('n is zero');
+    }
+}
+number(0);
