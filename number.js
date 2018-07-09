a96cdd2cb33279f35191e9ea21718a5cc745d542
sid3github <siddharthpadwal3@gmail.com>
2018-07-09 21:03:08 +0530

    added even odd gen

--- number.js
+++ number.js
@@ -1,13 +1,49 @@
-function number(n) {
+// <finding positive / negative number>
+// function number(n) {
+//
+//
+//     if (n < 0) {
+//         console.log('n is negative');
+//     }
+//     else if(n > 0) {
+//         console.log('n is positive');
+//     }else{
+//         console.log('n is zero');
+//     }
+// }
+// number(0);
 
 
-    if (n < 0) {
-        console.log('n is negative');
+//<finding even / odd >
+// function number (num) {
+//     if(num %2 == 0)
+//     {
+//         console.log('number is even');
+//     }else{
+//         console.log('number is odd');
+//     }
+//
+// }
+// number();
+
+
+//<Generate Even and Odd Number less than N>
+function number () {
+    for (let num = 0; num <= 20; num ++) {
+       if (num %2 == 0) {
+           console.log(num + 'even');
+       }
     }
-    else if(n > 0) {
-        console.log('n is positive');
-    }else{
-        console.log('n is zero');
-    }
+
 }
-number(0);
+number();
+
+function number () {
+    for (let num = 0; num <= 20; num ++) {
+        if (num %2 != 0) {
+            console.log(num + 'odd');
+        }
+    }
+
+}
+number();
