0819059bb6318a4e8c7a6484754f33298a463e3a
sid3github <siddharthpadwal3@gmail.com>
2018-07-20 21:42:09 +0530

    creating arrays from string

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-let array = [0,1,2,3];
-console.log(array.length);
+// simple array addition.
+let numbers = [1,2,3,44,55,66];
+let sum = 0;
+for(let i = 0; i < numbers.length; i++){
+  sum = sum + numbers[i];
+}
+console.log(sum);
