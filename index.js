49ed45296206fa84286c58fb3f6223a679df6398
sid3github <siddharthpadwal3@gmail.com>
2018-07-22 09:20:39 +0530

    ds-array manipulation.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 // simple array addition.
-let numbers = [1,2,3,44,55,66];
-let sum = 0;
-for(let i = 0; i < numbers.length; i++){
-  sum = sum + numbers[i];
-}
-console.log(sum);
+// let numbers = [1,2,3,44,55,66];
+// let sum = 0;
+// for(let i = 0; i < numbers.length; i++){
+//   sum = sum + numbers[i];
+// }
+// console.log(sum);
+
+
+// creating arrays from string.
+// let sentence = 'the hacking school';
+// let words = sentence.split('');
+// for(let i = 0; i < words.length; i++){
+//   console.log('words ' + i + ' :' + words[i] );
+// }
+
+//.push & .unshift.
+// function data ( arr ) {
+// arr.unshift ( 1, 'II', 'three' );
+// arr.push ( 'VII', 'seven', 8);
+// return arr;
+// };
+// console.log ( data ( ['four', 5, 'VI'] ));
